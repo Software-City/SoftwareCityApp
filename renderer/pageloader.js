2f75cf25d418f99dd366b52f3edafc9837b36b33
Software City <davissoftware6@gmail.com>
2021-01-17 15:48:09 +0100

    Delete pageloader.js

--- renderer/pageloader.js
+++
@@ -1,7 +0,0 @@
-function loadHTML(filename){
-    $("#mainpage").load("./../templates/" + filename);
-    // document.getElementById("mainpage").setAttribute("src", "./../templates/" + filename)
-}
-function loadURL(url){
-    $("#mainpage").load(url);
-}
